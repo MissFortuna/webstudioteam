b30963c7db9d500feabeb5041136cc78366e1909
anastasiasnzh <aastasuhan@gmail.com>
2018-04-03 00:04:40 +0300

    класс "отпуск"

---
+++ WebStudio/WebStudio/Models/Vacation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WebStudio.Classes
+{
+    public class Vacation
+    {
+        public string id { get; set; }
+        public DateTime start_date { get; set; }
+        public DateTime end_date { get; set; }
+        public virtual ICollection<Employee> employee { get; set; }
+        
+        public Vacation(string id, DateTime start_date, 
+            DateTime end_date, ICollection<Employee> employee)
+        {
+            this.id = id;
+            this.start_date = start_date;
+            this.end_date = end_date;
+            this.employee = employee;
+
+        }
+    }
+}
